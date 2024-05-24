3aca3458ef33f125c3a715c5b6106397f378ef1d
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-05-24 10:05:11 +0200

    modifiche varie exception custom

--- Exceptions/DeleteUserFailedException.cs
+++ Exceptions/DeleteUserFailedException.cs
@@ -2,6 +2,7 @@
 
 namespace Web_Application_Rest_Api.Exceptions {
 
+    [Serializable]
     public class DeleteUserFailedException : Exception, ISerializable {
 
         public DeleteUserFailedException(string message) : base(message) {
